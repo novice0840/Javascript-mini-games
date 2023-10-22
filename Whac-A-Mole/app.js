d3f8e830b031d8a3a92ef6ea31bd0c6fcfd7ad0c
novice0840 <magnolia0840@gmail.com>
2023-10-23 07:28:48 +0900

    게임 스코어 기록 완료

--- Whac-A-Mole/app.js
+++ Whac-A-Mole/app.js
@@ -1,4 +1,5 @@
-const moleNumber = 25;
+const MOLE_NUMBER = 25;
+const LIMIT_TIME = 30;
 const $moles = document.querySelectorAll(".mole");
 const $moleOn = document.querySelector(".mole-on");
 const $startButton = document.querySelector(".start-button");
@@ -6,7 +7,7 @@ const $timer = document.querySelector(".timer");
 const $score = document.querySelector(".score");
 
 const createRandomNumber = () => {
-  return Math.floor(Math.random() * moleNumber);
+  return Math.floor(Math.random() * MOLE_NUMBER);
 };
 
 const changeMole = () => {
@@ -20,15 +21,33 @@ const changeMole = () => {
   });
 };
 
-const startGame = () => {
-  changeMole();
-  $timer.innerText = 30;
-  $score.innerText = 0;
+const endGame = () => {
+  const name = prompt("닉네임을 입력해주세요");
+  localStorage.setItem(name, $score.innerText);
+  $timer.innerText = "";
+  $score.innerText = "";
 };
 
-$moleOn.addEventListener("click", () => {
-  console.log("mole on ");
+const startGame = () => {
   changeMole();
+  $timer.innerText = LIMIT_TIME;
+  $score.innerText = 0;
+  const timeInterval = setInterval(() => {
+    $timer.innerText = Number($timer.innerText) - 1;
+    if ($timer.innerText === "0") {
+      clearInterval(timeInterval);
+      endGame();
+    }
+  }, 1000);
+};
+
+$moles.forEach(($mole, index) => {
+  $mole.addEventListener("click", (event) => {
+    if ($mole.classList.contains("mole-on")) {
+      changeMole();
+      $score.innerText = Number($score.innerText) + 1;
+    }
+  });
 });
 
 $startButton.addEventListener("click", () => {
